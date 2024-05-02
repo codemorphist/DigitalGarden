aede6aaba702bbbad6b3c0f0553eda65c361ca4f
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2024-05-02 07:54:25 +0300

    Added app

---
+++ app/__main__.py
@@ -0,0 +1,6 @@
+from gui import App
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
---
+++ app/gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from plant_generator import Plant
+from tools import Color, Vec2
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
