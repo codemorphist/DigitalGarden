755dda763eacc922b762eaf95ac3ed68e83b5996
karbyshevillia <158076825+karbyshevillia@users.noreply.github.com>
2024-05-05 00:13:03 +0300

    Changes:
    branch test_circle for...testing circles
    
    tools.circle.py — added drawing method tied to canvas;
    
    app.gui.py — added get_plant() and draw() methods that allow to pick a TestPlant and draw its circles sequentially; also minor renaming.
    
    in other files changed relative import to absolute due to
    strange behaviour on my PC; subject to later change

--- app/gui.py
+++ app/gui.py
@@ -1,19 +1,77 @@
 import tkinter as tk
 from tkinter import ttk
+import turtle
 
 from plant_generator import Plant, TestPlant
 from tools import Color, Vec2, Circle
+
+
+class GenomTableFrame(ttk.Frame):
+    def __init__(self, container):
+        super().__init__(container)
+
+        self.genom_entries = {}
+
+        self.table_height = 9
+        self.table_width = 9
+        counter = 0
+        for row in range(self.table_height):
+            for column in range(self.table_width):
+                self.genom_entries[counter] = ttk.Entry(self, width=5)
+                self.genom_entries[counter].grid(padx=1, pady=1,
+                                                 row=row, column=column)
+                counter += 1
+
+        self.import_button = ttk.Button(self, text="Import")
+        self.export_button = ttk.Button(self, text="Export")
+
+        self.import_button.grid(row=self.table_height + 5, column=2)
+        self.export_button.grid(row=self.table_height + 5, column=6)
 
 
 class PlantGeneratorFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
 
+        self.canvas = tk.Canvas(master=self, width=800, height=800)
+        self.canvas.grid(padx=10, pady=10, row=0, column=0, rowspan=10, columnspan=10)
+        self.canvas_turtle = turtle.RawTurtle(self.canvas)
+
+        self.generate_button = tk.Button(master=self,
+                                         text="Generate",
+                                         command=self.draw)
+        self.generate_button.grid(padx=2,
+                                  pady=2,
+                                  row=1,
+                                  column=11,
+                                  sticky='nsew')
+
+        self.genom_input = GenomTableFrame(self)
+        self.genom_input.grid(row=0, column=11)
+
+    def get_plant(self):
+        return TestPlant(10)
+
     def draw(self):
-        pass            
+        """
+        Draws the TestPlant circles
+        """
+        test_plant = self.get_plant()
+        while test_plant.is_growing():
+            for circle in test_plant.get_circles():
+                circle.canvas_turtle_draw(self.canvas_turtle)
+
 
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        self.title("Digital Garden")
+        self.geometry("1280x900")
+
+        self.__create_widgets__()
+
+    def __create_widgets__(self):
+        generate_frame = PlantGeneratorFrame(self)
+        generate_frame.grid(column=0, row=0)
--- tools/tools/circle.py
+++ tools/tools/circle.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
-from .vector import Vec2
-from .color import Color
+
+import turtle
+
+from tools.vector import Vec2
+from tools.color import Color
 from random import randint
 
 
@@ -14,12 +17,29 @@ class Circle:
     def random() -> Circle:
         return Circle(
             Vec2.random(),
-            randint(1, 20),
+            randint(10, 20),
             Color.random()
         )
 
     def __repr__(self) -> str:
         return f"Circle(position={self.pos}, radius={self.radius}, color={self.color})"
              
+    def canvas_turtle_draw(self, canvas_turtle):
+        """
 
+        This Circle method draws a given circle with a given turtle,
+        which is thought as tied to a specific canvas; the parameters of
+        the drawn circle match those of the Object.
 
+        """
+        canvas_turtle.speed(0)
+        # turtle.colormode(255)
+        canvas_turtle.penup()
+        canvas_turtle.goto(self.pos.x, self.pos.y)
+        canvas_turtle.forward(self.radius)
+        canvas_turtle.left(90)
+        canvas_turtle.pendown()
+        # canvas_turtle.pencolor(self.color.rgb)
+        canvas_turtle.circle(self.radius)
+        canvas_turtle.penup()
+        canvas_turtle.goto(self.pos.x, self.pos.y)
