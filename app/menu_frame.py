1bbfc5d46a32c3d4fe22def6415cebce38502810
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2024-05-13 11:49:30 +0300

    Implemented menu window

--- app/menu_frame.py
+++ app/menu_frame.py
@@ -3,12 +3,32 @@ from tkinter import ttk
 from generator_frame import PlantGenerator
 
 
+class MenuButtonsFrame(ttk.Frame):
+    def __init__(self, containter, buttons: dict[str, callable]):
+        super().__init__(containter)
+
+        self.buttons = {}
+        for text, callback in buttons.items():
+            self.buttons[text] = ttk.Button(self, text=text, 
+                                            command=callback)
+        self.configure_buttons()
+
+    def configure_buttons(self):
+        for row, btn in enumerate(self.buttons):
+            self.buttons[btn].grid(row=row, column=0, padx=5, pady=5,
+                        sticky="nsew")
+
 class Menu(ttk.Frame):
     def __init__(self, containter, controller):
         super().__init__(containter)
-         
-        self.generate_btn = ttk.Button(self, text="Generate Plant",
-        command = lambda: controller.show_frame(PlantGenerator))
+        self.controller = controller
+
+        buttons = {
+            "Generate Plant": lambda: self.controller.show_frame(PlantGenerator),
+            "Smash Plant": lambda: None,
+            "Exit": lambda: self.controller.destroy()
+        }
+        self.buttons_frame = MenuButtonsFrame(self, buttons)
 
         self.configure_widgets()
      
@@ -16,5 +36,5 @@ class Menu(ttk.Frame):
         """
         Self-explanatory
         """
-        self.generate_btn.grid(row=1, column=1, padx=10, pady=10)
-  
+        self.buttons_frame.place(relx=0.5, rely=0.5, 
+                                 anchor=tk.CENTER) 
