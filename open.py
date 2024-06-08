ad6262b84b302030f93685c27452cfe093d4466b
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2024-06-08 23:50:33 +0300

    Test hash

--- open.py
+++ open.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import hashlib
 from plant_generator import PlantGenom
 
 # Load the image with metadata
@@ -6,6 +7,13 @@ image = Image.open("./fdasfda.png")
 
 # Retrieve the metadata
 metadata = image.info
+name = metadata["Plant"]
+genom = metadata["Genom"]
+
+hash_string = name + genom.replace(" ", "/").replace("\n","|")
+print(hash_string)
+hash = hashlib.sha256(hash_string.encode("utf-8")).hexdigest()
+print(hash == metadata["PlantHash"]) 
 
 print(metadata)
 print(PlantGenom.import_genom(metadata["Genom"]))
